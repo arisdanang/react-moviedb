0902bd21161f25f7e443c89cd5d1d69f13fccca3
arisdanang <arisdanang2810@gmail.com>
2024-01-26 14:02:56 +0700

    fix: design card and fetch api tmdb

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,30 @@
-const Card = () => {
-  return <div></div>;
+import PropTypes from "prop-types";
+
+const Card = ({ title, imageUrl, desc, year }) => {
+  return (
+    <div className="max-w-sm border rounded-md p-4">
+      <img
+        src={`https://image.tmdb.org/t/p/original/${imageUrl}`}
+        alt="poster"
+        className="w-full"
+        loading="lazy"
+      />
+      <h1>{title}</h1>
+      <h2>{year}</h2>
+      <p>{desc}</p>
+      <div className="flex justify-between mt-4">
+        <button className="px-4 py-2 border rounded-md">more Info</button>
+        <button className="px-4 py-2 border rounded-md">Add to list</button>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
 };
 
 export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,27 +5,38 @@ import axios from "axios";
 const Home = () => {
   const token =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjcwYTVlMGI2YmYzNDM0NTMxMWMxNTI1MGVlNWZjZiIsInN1YiI6IjY1YjJhNDkyNmVlY2VlMDE3YjM0ZWVkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BskjHUJ2-4XHFwfqm9SPMaaDpN6H7-lhyDHAoAvX_cE";
-  const { isLoading, isError, data, error, refetch } = useQuery({
-    queryKey: ["movie"],
-    queryFn: () => {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-          {
-            headers: {
-              accept: "application/json",
-              Authorization: `Bearer ${token} `,
-            },
-          }
-        )
-        .then((res) => res.data);
-    },
+  const getMovies = async () => {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return data;
+  };
+  const { data } = useQuery({
+    queryKey: ["movies"],
+    queryFn: getMovies,
   });
 
   console.log("data", data);
+
   return (
-    <div>
-      <Card />
+    <div className="max-w-[68rem] w-[91%] mx-auto">
+      <div className="grid grid-cols-3">
+        {data?.results?.map((result) => (
+          <Card
+            title={result?.title}
+            key={result?.id}
+            imageUrl={result?.poster_path}
+            desc={result?.overview}
+          />
+        ))}
+      </div>
     </div>
   );
 };
